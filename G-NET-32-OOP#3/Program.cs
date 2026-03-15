b33b289a2a16cbe869f2583712ac9d24ad2f5c29
omar0200 <omarminato17@gmail.com>
2026-03-15 14:39:41 +0200

    Question 2

--- G-NET-32-OOP#3/Program.cs
+++ G-NET-32-OOP#3/Program.cs
@@ -15,6 +15,18 @@
              e) dependency
              */
             #endregion
+
+            #region Question 2
+            /*
+             a) yes it will be accessible ,no
+             b) * private is only accessible within the class
+                * protected is accessible within the class and its derived classes
+                * internal is accessible within the same assembly
+                * public is accessible from anywhere
+             c) it means it cant be drived , and method cant be override
+            d) yes we can , because seal prevents inheritance but not object creation
+            */
+            #endregion
             #endregion
         }
     }

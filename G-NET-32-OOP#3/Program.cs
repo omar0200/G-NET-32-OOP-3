97eb29d810ee7345082e08708bf5f177fbfe41ea
omar0200 <omarminato17@gmail.com>
2026-03-15 14:17:45 +0200

    Add project files.

---
+++ G-NET-32-OOP#3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G_NET_32_OOP_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
